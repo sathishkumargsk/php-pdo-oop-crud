4f2f54f444215feb13e6158cc4748ea3b45b2079
satish kumar <satish@blr-ad2.systech.sysfore.com>
2016-11-28 16:21:23 +0530

    insert file edited

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # php-pdo-oop-crud
 A simple and latest php crud with pdo oop
+
+#For Database Creation
+
+CREATE TABLE `customers` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+ `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+ `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ `created` datetime NOT NULL,
+ `modified` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- add_customer.php
+++ add_customer.php
@@ -1,10 +1,45 @@
 <html>
 	<head>
 		<title>PHP CRUD PDO OOP</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 	</head>
 	<body>
-		<div>
-		<input type="text">
+		<div class="container">
+		<div classs="row ">
+		<h2>Add customer</h2>
+		<form method="post" action="" class="col-md-6">
+		<div class="form-group">
+		<label for="name">Name:</label>
+		<input type="text" class="form-control" required>
+        </div>
+		
+		<div class="form-group">
+		<label for="email">Email:</label>
+		<input type="email" class="form-control" required>
 		</div>
+
+		<div class="form-group">
+		<label for="phone">Phone:</label>
+		<input type="phone" class="form-control" required>
+		</div>
+        
+        <div class="form-group">
+        <label for="image">Image:</label>
+        <input type="file" class="form-control" required>
+        </div> 
+      
+        <div class="form-group">
+        <button type="submit" class="btn btn-warning">Submit</button>
+        </div>
+        </form>
+        </div>
+        </div>
+
+
+        <script src="js/jquery-3.1.1.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
